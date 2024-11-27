1cf0419f87c85e8ba119a7ab16eb39f66dcb6fdf
Jasmine Kobayashi <jasmine.kobayashi@swri.org>
2024-11-27 16:42:49 -0700

    add way to randomize date range of test data to look at

--- tsFB/filterbank_analysis.py
+++ tsFB/filterbank_analysis.py
@@ -14,6 +14,7 @@ import dill as pickle
 from mpl_toolkits.mplot3d import Axes3D 
 
 from scipy import fft
+import random as rnd
 
 import datetime as dt
 import os,sys
@@ -45,13 +46,44 @@ parser.add_argument(
 )
 parser.add_argument(
     '-start_date',
-    default='2018-11-21',
-    help='Start date for interval. Defaults to 2018-11-21.'
+    default=None,
+    help='Start date for interval.'
+    'If None, will use values from args `start_year`, `start_month`, and `start_day`'
 )
 parser.add_argument(
     '-stop_date',
-    default='2018-12-31',
+    default=None,
     help='Stop date for interval. Defaults to 2018-12-31.'
+)
+parser.add_argument(
+    '-start_year',
+    default=None,
+    help='Start year for interval.'
+    'If None, value is randomized to value between 1994 and 2023.'
+    'Defaults to None.'
+)
+parser.add_argument(
+    '-start_month',
+    default=None,
+    help='Start month for interval.'
+    'If None, value is randomized.'
+    'Defaults to None.'
+)
+parser.add_argument(
+    '-start_day',
+    default=None,
+    help='Start day for interval.'
+    'If None, value is randomized to value between 1 and 28.'
+    'Defaults to None.'
+)
+parser.add_argument(
+    '-chunk_size',
+    default=86400,
+    help=(
+        'Duration, in seconds, length of test data'
+        'Defaults to 86400 seconds (1 day).'
+    ),
+    type=int
 )
 parser.add_argument(
     '-cadence',
@@ -171,7 +203,8 @@ def view_filter_decomposition(data_df,
                                      gs_hspace = 0,
                                      xlim = None,
                                      center_freq = None,
-                                     show_plot=True):
+                                     filterbank_plot_title='Filter bank',
+                                     orig_sig_date=''):
     """Plot comprehensive visualization of filterbank and its application to a set of test data.
     Plot includes the filterbank, raw test data, decomposition of filterbank preprocessed data.
     
@@ -214,9 +247,9 @@ def view_filter_decomposition(data_df,
 
     ax0 = fig.add_subplot(os_gs)   
     ax0.plot(x, y)
-    ax0.set_title(f'Original series ({data_col})')
-    ax0.set_xticks([])
-    ax0.set_yticks([])
+    ax0.set_title(orig_sig_date+f'Original series ({data_col})')
+    # ax0.set_xticks([])
+    # ax0.set_yticks([])
 
     if xlim is None:
         xlim = (fftfreq[0],fftfreq[-1])
@@ -226,38 +259,56 @@ def view_filter_decomposition(data_df,
     # ax.set_ylabel('Weight')
     ax.set_xlabel('Frequency (Hz)')
     ax.set_xlim(xlim)
-    ax.set_title('Mel filter bank')
+    ax.set_title(filterbank_plot_title)
     ax.set_xticks(center_freq)
     ax.ticklabel_format(style='sci',scilimits=(0,0),axis='x')
-    if show_plot:
-        plt.show()
-    else:
-        plt.close()
+    plt.show()
     
 
 if __name__ == '__main__':
     # args--------------------------------------------------
     args = vars(parser.parse_args())
-    args['start_date'] = dt.datetime.strptime(
+    if args['start_date'] is None:
+        if args['start_year'] is None:
+            args['start_year'] = rnd.randint(1981,2023)
+        if args['start_month'] is None:
+            args['start_month'] = format(rnd.randint(1,12),'02')
+        if args['start_day'] is None:
+            args['start_day'] = format(rnd.randint(1,28),'02')
+        args['start_date'] = dt.datetime.strptime(
+            f'{args['start_year']}-{args['start_month']}-{args['start_day']}',
+            '%Y-%m-%d'
+        )
+    else:
+        args['start_date'] = dt.datetime.strptime(
         args['start_date'],
         '%Y-%m-%d'
     )
-    args['stop_date'] = dt.datetime.strptime(
-        args['stop_date'],
-        '%Y-%m-%d'
-    )
+        
+    args['chunk_size'] = dt.timedelta(seconds=args['chunk_size'])
+
+    if args['stop_date'] is None:
+        args['stop_date'] = args['start_date'] + args['chunk_size']
+    else:
+        args['stop_date'] = dt.datetime.strptime(
+            args['stop_date'],
+            '%Y-%m-%d'
+        )
+
     args['cadence'] = dt.timedelta(seconds=args['cadence'])
     # -------------------------------------------------------
 
     # Test data
     if args['input_file'] is None:
-        year = '2019'
-        month = '05'
+        year = str(args['start_year'])
+        month = str(args['start_month'])
         test_cdf_file_path =_SRC_DIR+_OMNI_MAG_DATA_DIR+ year +'/omni_hro_1min_'+ year+month+'01_v01.cdf'
     else:
         test_cdf_file_path = args['input_file']
     
-    mag_df = get_test_data(fname_full_path=test_cdf_file_path)
+    mag_df = get_test_data(fname_full_path=test_cdf_file_path,
+                           start_date=args['start_date'],
+                           end_date=args['stop_date'])
     mag_df = mag_df-mag_df.mean()
 
     # Build Filterbank
@@ -285,4 +336,5 @@ if __name__ == '__main__':
                                      data_col=col,
                                      cadence=dt.timedelta(minutes=1),
                                      xlim = (fltbnk.edge_freq[0],fltbnk.edge_freq[-1]),
-                                     center_freq = fltbnk.center_freq)
+                                     center_freq = fltbnk.center_freq,
+                                     orig_sig_date=f'[{args["start_year"]}-{args['start_month']}-{args['start_day']}] ')
