c1f1b9def9b4e851b1b418c0bf22fc2ce427b617
Jasmine Kobayashi <jasmine.kobayashi@swri.org>
2025-06-26 14:57:18 -0600

    add way to test the code with sample data

--- tsFB/build_filterbanks.py
+++ tsFB/build_filterbanks.py
@@ -8,6 +8,8 @@ import dill as pickle
 import datetime as dt
 import os,sys
 
+import argparse
+
 _MODEL_DIR = os.path.dirname(os.path.abspath(__file__))
 _SRC_DIR = os.path.dirname(_MODEL_DIR)
 sys.path.append(_SRC_DIR)
@@ -15,6 +17,83 @@ sys.path.append(_SRC_DIR)
 # Data paths
 _SRC_DATA_DIR = os.path.join(_SRC_DIR,'data',)
 
+
+# Debugger arguments
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-input_file',
+    default=None,
+    help='direct path to file to use for test'
+)
+parser.add_argument(
+    '-start_date',
+    default=None,
+    help='Start date for interval.'
+    'If None, will use values from args `start_year`, `start_month`, and `start_day`'
+)
+parser.add_argument(
+    '-stop_date',
+    default=None,
+    help='Stop date for interval. Defaults to 2018-12-31.'
+)
+parser.add_argument(
+    '-start_year',
+    default=None,
+    help='Start year for interval.'
+    'If None, value is randomized to value between 1994 and 2023.'
+    'Defaults to None.'
+)
+parser.add_argument(
+    '-start_month',
+    default=None,
+    help='Start month for interval.'
+    'If None, value is randomized.'
+    'Defaults to None.'
+)
+parser.add_argument(
+    '-start_day',
+    default=None,
+    help='Start day for interval.'
+    'If None, value is randomized to value between 1 and 28.'
+    'Defaults to None.'
+)
+parser.add_argument(
+    '-chunk_size',
+    default=86400,
+    help=(
+        'Duration, in seconds, length of test data'
+        'Defaults to 86400 seconds (1 day).'
+    ),
+    type=int
+)
+parser.add_argument(
+    '-cadence',
+    default=1,
+    help=(
+        'Final cadence of interpolated timeseries in seconds.'
+        'Defaults to 1 second.'
+    ),
+    type=int
+)
+parser.add_argument(
+    '-absolute_residual',
+    help='Whether or not to use absolute value of residuals',
+    default=True,
+    action='store_true'
+)
+parser.add_argument(
+    '-residual_epsilon',
+    help='Epsilon in denominator of relative residual calculation (to minimize effect of dividing by near zero).',
+    default=0.01,
+    type=float
+)
+
+def list_of_strings(arg):
+    return arg.split(',')
+
+parser.add_argument('-cols', 
+                    type=list_of_strings,
+                    default=['B_mag','BX_GSE','BY_GSE','BZ_GSE'])
 
 def moving_avg_freq_response(f,
                              window=dt.timedelta(minutes=3000),
@@ -414,3 +493,75 @@ class filterbank:
 
         with open(_SRC_DATA_DIR + '/filterbanks/' + fb_prefix +'.pkl', 'wb') as f:
             pickle.dump(filterbank_dictionary,f)
+
+if __name__ == '__main__':
+    import random as rnd
+    import tsFB.filterbank_analysis as fa
+    # args==============================================================
+    args = vars(parser.parse_args())
+    if args['start_date'] is None:
+        if args['start_year'] is None:
+            args['start_year'] = rnd.randint(1981,2023)
+        if args['start_month'] is None:
+            args['start_month'] = format(rnd.randint(1,12),'02')
+        if args['start_day'] is None:
+            args['start_day'] = format(rnd.randint(1,28),'02')
+        args['start_date'] = dt.datetime.strptime(
+            f'{args['start_year']}-{args['start_month']}-{args['start_day']}',
+            '%Y-%m-%d'
+        )
+    else:
+        args['start_date'] = dt.datetime.strptime(
+        args['start_date'],
+        '%Y-%m-%d'
+    )
+        
+    args['chunk_size'] = dt.timedelta(seconds=args['chunk_size'])
+
+    if args['stop_date'] is None:
+        args['stop_date'] = args['start_date'] + args['chunk_size']
+    else:
+        args['stop_date'] = dt.datetime.strptime(
+            args['stop_date'],
+            '%Y-%m-%d'
+        )
+
+    args['cadence'] = dt.timedelta(seconds=args['cadence'])
+    
+
+    # Test data=========================================================
+    mag_df = fa.get_test_data(start_date=args['start_date'],
+                               end_date=args['stop_date'],
+                               cols=args['cols'])
+    # mag_df = mag_df-mag_df.mean()
+
+    # Build Filterbank
+    fltbnk = filterbank(data_len=len(mag_df),
+                           cadence=dt.timedelta(seconds=60))
+    fltbnk.build_triangle_fb(filter_freq_range=(0.01,0.04),
+                             center_freq=[0.02,0.03],
+                             freq_units='sample_rate_frac'
+                             )
+    # fb.visualize_filterbank(fb_matrix=fltbnk.fb_matrix,
+    #                      fftfreq=fltbnk.freq_spectrum['hertz'],
+    #                      xlim=(fltbnk.edge_freq[0],fltbnk.edge_freq[-1]),
+    #                      ylabel='Amplitude')
+    fltbnk.add_DC_HF_filters()
+    visualize_filterbank(fb_matrix=fltbnk.fb_matrix,
+                         fftfreq=fltbnk.freq_spectrum['sample_rate_frac'],
+                         xlim=(fltbnk.edge_freq[0]-0.005,fltbnk.edge_freq[-1]+0.005),
+                         ylabel='Amplitude',)
+    
+    import matplotlib.colors as mcolors
+    for i,bank in enumerate(fltbnk.fb_matrix):
+        fig,ax = plt.subplots(figsize=(8,3))
+        ax.plot(fltbnk.freq_spectrum['sample_rate_frac'],bank,color = list(mcolors.TABLEAU_COLORS.keys())[i])
+        ax.grid(True)
+        ax.set_ylabel(ylabel='Amplitude')
+        ax.set_xlabel('Frequency')
+        # if xlim is None:
+        #     xlim = (np.min(fftfreq),np.max(fftfreq))
+        ax.set_xlim(0.0005,0.045)
+
+        plt.tight_layout()
+        plt.show()
